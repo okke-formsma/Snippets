05fb39347f7f39e6bb5fa9e9f6187c938b51ba22
Okke Formsma <okke.formsma@gmail.com>
2012-02-16 14:59:38 +0100

    empty

---
+++ utils.py
@@ -0,0 +1,6 @@
+sprinter(iterable, every=1000):
+    length = len(iterable)
+    for i, row in enumerate(iterable):
+        if i % every == 0:
+            print i, 'of', length, row
+        yield row
