9cb7ef1dd4676e633619f1bbb6f47b170116dbd2
Okke Formsma <okke.formsma@gmail.com>
2012-05-03 10:16:21 +0200

    Created @main decorator, updated default args for progressprinter

--- utils.py
+++ utils.py
@@ -1,6 +1,13 @@
-def progressprinter(iterable, every=1000):
+def progressprinter(iterable, every=1):
+	""" Prints a progress counter like "1 of 1000" . """
     length = len(iterable)
     for i, row in enumerate(iterable):
         if i % every == 0:
             print i, 'of', length, row
         yield row
+
+def main(func):
+	""" Decorator which evokes the function if the script was run from the commandline """
+	if __name__ == '__main__':
+		func()
+	return func
